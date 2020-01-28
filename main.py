8c6371bc9021aead8638c087547177dc182df3d2
MatchaTeaLeaf <matchawookiee@gmail.com>
2020-01-28 20:15:57 +0800

    Added logging to discord.log

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import keep_alive
+import logging
 
 # Change adminUsers to your discord username (can have mutliple adminUsers)
 # adminUsers gets to load/unload/reload cogs
@@ -9,6 +10,13 @@ import keep_alive
 adminUsers = ['username#1234']
 commandPrefix = ['emilie ', 'Emilie ']
 client = commands.Bot(command_prefix=commandPrefix)
+
+# Set up logging to discord.log
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 keep_alive.keep_alive()
 
